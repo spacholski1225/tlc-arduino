bfc2b31a48ac353c816e0cee163e12dc7b75187b
unknown <spacholski99@gmail.com>
2022-07-27 20:52:50 +0200

    01-created logic for  setting up diodes

--- TrafficLightSimulator/TrafficLightSimulator.ino
+++ TrafficLightSimulator/TrafficLightSimulator.ino
@@ -1,9 +1,54 @@
-void setup() {
-  // put your setup code here, to run once:
+#define RED_L 2
+#define YELLOW_L 3
+#define GREEN_L 4
 
+#define RED_U 5
+#define YELLOW_U 6
+#define GREEN_U 7
+
+#define RED_B A5
+#define YELLOW_B A4
+#define GREEN_B A3
+
+#define RED_R 8
+#define YELLOW_R 9
+#define GREEN_R 10
+
+int diodes[] = { RED_L, YELLOW_L, GREEN_L, RED_U, YELLOW_U, GREEN_U, RED_B, YELLOW_B, GREEN_B, RED_R, YELLOW_R, GREEN_R };
+
+int diodesLength;
+
+void setup() 
+{
+  diodesLength = sizeof(diodes)/sizeof(*diodes);
+
+  Serial.begin(9600);
+  
+  Serial.println("setup");
+  setPinModes();
+  turnOnDiodes();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop() 
+{
+}
 
+void setPinModes()
+{
+  Serial.println(diodesLength);
+  for(int i = 0; i < diodesLength; i++)
+  {
+    Serial.println(i);
+    pinMode(diodes[i], OUTPUT);
+    delay(1);
+  }
+}
+
+void turnOnDiodes()
+{  
+  for(int i = 0; i < diodesLength; i++)
+  {
+    digitalWrite(diodes[i], HIGH);
+    delay(100);
+  }
 }
