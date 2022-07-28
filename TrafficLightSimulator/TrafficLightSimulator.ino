949798171e3e5bf7a59485a246a7a9fd0bec4ea5
unknown <spacholski99@gmail.com>
2022-07-28 22:58:55 +0200

    01 - Finished first traffic light, left and right workis correctly, but up and bottom are synchonized ie are plug in to same pins

--- TrafficLightSimulator/TrafficLightSimulator.ino
+++ TrafficLightSimulator/TrafficLightSimulator.ino
@@ -2,41 +2,51 @@
 #define YELLOW_L 3
 #define GREEN_L 4
 
-#define RED_U 5
-#define YELLOW_U 6
-#define GREEN_U 7
-
-#define RED_B A5
-#define YELLOW_B A4
-#define GREEN_B A3
+#define RED_U_B A5
+#define YELLOW_U_B A4
+#define GREEN_U_B A3
 
 #define RED_R 8
 #define YELLOW_R 9
 #define GREEN_R 10
 
-int diodes[] = { RED_L, YELLOW_L, GREEN_L, RED_U, YELLOW_U, GREEN_U, RED_B, YELLOW_B, GREEN_B, RED_R, YELLOW_R, GREEN_R };
+int diodes[] = {RED_L, YELLOW_L, GREEN_L, RED_U_B, YELLOW_U_B, GREEN_U_B, RED_R, YELLOW_R, GREEN_R};
 
-void setup() 
+void setup()
 {
- 
 
   Serial.begin(9600);
-  
+
   Serial.println("setup");
   setPinModes();
   setStartParameters();
 }
 
-void loop() 
+void loop()
 {
+  delay(2000);//waiting for cars to go left-right
+  turnOnRedDiode(GREEN_L, YELLOW_L, RED_L);
+  turnOnRedDiode(GREEN_R, YELLOW_R, RED_R);
+
+  delay(2000);
+  turnOnGreenDiode(GREEN_U_B, YELLOW_U_B, RED_U_B); // waiting for cars to go up-bottom
+
+  delay(2000);
+  turnOnRedDiode(GREEN_U_B, YELLOW_U_B, RED_U_B);
+
+  delay(2000);
+  turnOnGreenDiode(GREEN_L, YELLOW_L, RED_L);
+  turnOnGreenDiode(GREEN_R, YELLOW_R, RED_R);
+
+
 }
 
 void setPinModes()
 {
-  int diodesLength = sizeof(diodes)/sizeof(*diodes);
+  int diodesLength = sizeof(diodes) / sizeof(*diodes);
 
   Serial.println(diodesLength);
-  for(int i = 0; i < diodesLength; i++)
+  for (int i = 0; i < diodesLength; i++)
   {
     Serial.println(i);
     pinMode(diodes[i], OUTPUT);
@@ -46,13 +56,13 @@ void setPinModes()
 
 void turnOnRedDiode(int greenDiode, int yellowDiode, int redDiode)
 {
-  if(digitalRead(greenDiode) == HIGH)
+  if (digitalRead(greenDiode) == HIGH)
   {
     digitalWrite(greenDiode, LOW);
     delay(200);
   }
-  
-  if(digitalRead(yellowDiode) == LOW && digitalRead(redDiode) == LOW)
+
+  if (digitalRead(yellowDiode) == LOW && digitalRead(redDiode) == LOW)
   {
     digitalWrite(yellowDiode, HIGH);
     delay(2000);
@@ -63,14 +73,14 @@ void turnOnRedDiode(int greenDiode, int yellowDiode, int redDiode)
 
 void turnOnGreenDiode(int greenDiode, int yellowDiode, int redDiode)
 {
-  if(digitalRead(redDiode) == HIGH)
+  if (digitalRead(redDiode) == HIGH)
   {
     digitalWrite(redDiode, LOW);
     digitalWrite(yellowDiode, HIGH);
     delay(500);
   }
 
-   if(digitalRead(redDiode) == LOW && digitalRead(yellowDiode) == HIGH)
+  if (digitalRead(redDiode) == LOW && digitalRead(yellowDiode) == HIGH)
   {
     digitalWrite(yellowDiode, LOW);
     digitalWrite(greenDiode, HIGH);
@@ -78,9 +88,8 @@ void turnOnGreenDiode(int greenDiode, int yellowDiode, int redDiode)
 }
 
 void setStartParameters()
-{  
+{
   digitalWrite(GREEN_L, HIGH);
   digitalWrite(GREEN_R, HIGH);
-  digitalWrite(RED_U, HIGH);
-  digitalWrite(RED_B, HIGH);
+  digitalWrite(RED_U_B, HIGH);
 }
