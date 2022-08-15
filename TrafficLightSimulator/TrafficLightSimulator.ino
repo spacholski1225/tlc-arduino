63d9bdf4a566b568ad34a52aa5cf261e7754af9c
unknown <spacholski99@gmail.com>
2022-08-15 15:49:37 +0200

    05- created changing light after button click- code needs refactor

--- TrafficLightSimulator/TrafficLightSimulator.ino
+++ TrafficLightSimulator/TrafficLightSimulator.ino
@@ -10,31 +10,41 @@
 #define EXPANDER2_RED_L 6
 #define EXPANDER2_YELLOW_L 5
 #define EXPANDER2_GREEN_L 4
-#define EXPANDER2_RED_B 1
+#define EXPANDER2_RED_B 3
 #define EXPANDER2_YELLOW_B 2
-#define EXPANDER2_GREEN_B 3
+#define EXPANDER2_GREEN_B 1
 
-PCF8574 expander1;
-PCF8574 expander2;
+#define BUTTON_CROSS_L 10
+#define BUTTON_CROSS_B 11
+
+PCF8574 crossExpander;
+PCF8574 lightExpander;
 
 const int expander1Diodes[] = {EXPANDER1_RED_CROSS_L, EXPANDER1_GREEN_CROSS_L, EXPANDER1_RED_CROSS_B, EXPANDER1_GREEN_CROSS_B};
 const int expander2Diodes[] = {EXPANDER2_RED_L, EXPANDER2_YELLOW_L, EXPANDER2_GREEN_L, EXPANDER2_RED_B, EXPANDER2_YELLOW_B, EXPANDER2_GREEN_B};
 
-TrafficLight traffic(expander1, expander2);
+TrafficLight traffic(crossExpander, lightExpander);
 
-bool isDayMode = true;
+bool isDayMode = false;
+
+bool isOnLeftCross = false;
+bool isOnBottomCross = true;
 
 void setup()
 {
-  expander1.begin(0x20);
-  expander2.begin(0x21);
+  crossExpander.begin(0x20);
+  lightExpander.begin(0x21);
+
+  pinMode(BUTTON_CROSS_L, INPUT_PULLUP);
+  pinMode(BUTTON_CROSS_B, INPUT_PULLUP);
 
   Serial.begin(9600);
 
+  setUpExpanderDiodes();
 
   if (isDayMode) // to change cuz it run only once time, so always will execute setdDayModeStartParameters()
   {
-    setDayModeStartParameters();
+    // setDayModeStartParameters();
   }
   else
   {
@@ -43,81 +53,138 @@ void setup()
 }
 
 void loop()
-{ 
+{
+  turnOnNightMode();
+}
 
+void turnOnCrossLightAfterButtonClick(int crossGreenDiode, int crossRedDiode, int lightGreenDiode, int lightYellowDiode, int lightRedDiode)
+{
+  crossExpander.digitalWrite(crossRedDiode, LOW);
+  crossExpander.digitalWrite(crossGreenDiode, HIGH);
+
+  turnOnRedDiodeAndOffGreen(lightGreenDiode, lightYellowDiode, lightRedDiode);
+
+  delay(5000);
+
+  crossExpander.digitalWrite(crossRedDiode, HIGH);
+  crossExpander.digitalWrite(crossGreenDiode, LOW);
+
+  turnOnGreenDiodeAndOffRed(lightGreenDiode, lightYellowDiode, lightRedDiode);
 }
 
 void setUpExpanderDiodes()
 {
-   int expanderDiodesLength = sizeof(expander1Diodes) / sizeof(*expander1Diodes);
+  int expanderDiodesLength = sizeof(expander1Diodes) / sizeof(*expander1Diodes);
   for (int index = 0; index < expanderDiodesLength; index++)
   {
-    expander1.pinMode(expander1Diodes[index], OUTPUT);
-  } 
+    crossExpander.pinMode(expander1Diodes[index], OUTPUT);
+  }
 
   int expander2DiodesLength = sizeof(expander2Diodes) / sizeof(*expander2Diodes);
   for (int index = 0; index < expander2DiodesLength; index++)
   {
-    expander2.pinMode(expander2Diodes[index], OUTPUT);
+    lightExpander.pinMode(expander2Diodes[index], OUTPUT);
   }
 }
 
 void turnOnNightMode()
 {
-  setNightModeStartParameters();
+  if (digitalRead(BUTTON_CROSS_L) == LOW)
+  {
+    isOnLeftCross = true;
+  }
+  if (digitalRead(BUTTON_CROSS_B) == LOW)
+  {
+    isOnBottomCross = true;
+  }
+
+  if (isOnLeftCross)
+  {
+    turnOnCrossLightAfterButtonClick(EXPANDER1_GREEN_CROSS_L, EXPANDER1_RED_CROSS_L, EXPANDER2_GREEN_L, EXPANDER2_YELLOW_L, EXPANDER2_RED_L);
+    isOnLeftCross = false;
+  }
 }
 
-void turnOnDayMode()
+void turnOnRedDiodeAndOffGreen(int greenDiode, int yellowDiode, int redDiode) // to jest zjebana metoda trzeba ja przerobic!
 {
-  delay(2000); // waiting for cars to go left-right
-  traffic.turnOnRedDiodeAndOffGreen(GREEN_L, YELLOW_L, RED_L);
-  traffic.turnOnGreenZebraCrossControlledByTime(GREEN_CROSS_CENTER, RED_CROSS_CENTER);
-  traffic.turnOnRedDiodeAndOffGreen(GREEN_R, YELLOW_R, RED_R);
+  if (lightExpander.digitalRead(greenDiode) == HIGH)
+  {
+    lightExpander.digitalWrite(greenDiode, LOW);
+    delay(200);
+  }
 
-  // turn on zebra lights
-  traffic.turnOnGreenZebraCrossControlledByTime(GREEN_CROSS_L, RED_CROSS_L);
-  traffic.turnOnRedZebraCrossControlledByTime(GREEN_CROSS_U, RED_CROSS_U);
+  if (lightExpander.digitalRead(yellowDiode) == LOW && lightExpander.digitalRead(redDiode) == LOW)
+  {
+    lightExpander.digitalWrite(greenDiode, LOW); // only to check
 
-  delay(2000);
-  traffic.turnOnGreenDiodeAndOffRed(GREEN_U_B, YELLOW_U_B, RED_U_B); // waiting for cars to go up-bottom
-
-  delay(2000);
-  traffic.turnOnRedDiodeAndOffGreen(GREEN_U_B, YELLOW_U_B, RED_U_B);
-
-  traffic.turnOnGreenZebraCrossControlledByTime(GREEN_CROSS_U, RED_CROSS_U);
-  traffic.turnOnRedZebraCrossControlledByTime(GREEN_CROSS_L, RED_CROSS_L);
-
-  delay(2000);
-  traffic.turnOnRedZebraCrossControlledByTime(GREEN_CROSS_CENTER, RED_CROSS_CENTER);
-  traffic.turnOnGreenDiodeAndOffRed(GREEN_L, YELLOW_L, RED_L);
-  traffic.turnOnGreenDiodeAndOffRed(GREEN_R, YELLOW_R, RED_R);
+    lightExpander.digitalWrite(yellowDiode, HIGH);
+    delay(2000);
+    lightExpander.digitalWrite(yellowDiode, LOW);
+    lightExpander.digitalWrite(redDiode, HIGH);
+  }
 }
 
-void setDayModeStartParameters()
+void turnOnGreenDiodeAndOffRed(int greenDiode, int yellowDiode, int redDiode)
 {
-  // setting traffic light
-  digitalWrite(GREEN_L, HIGH);
-  digitalWrite(GREEN_R, HIGH);
-  digitalWrite(RED_U_B, HIGH);
 
-  // setting zebra cross for left up
-  digitalWrite(RED_CROSS_L, HIGH);
+  lightExpander.digitalWrite(redDiode, LOW);
+  lightExpander.digitalWrite(yellowDiode, HIGH);
+  delay(500);
 
-  // setting zebra cross for down
-  digitalWrite(GREEN_CROSS_U, HIGH);
-
-  // setting center zebra cross
-  digitalWrite(RED_CROSS_CENTER, HIGH);
+  lightExpander.digitalWrite(yellowDiode, LOW);
+  lightExpander.digitalWrite(greenDiode, HIGH);
 }
+
+// void turnOnDayMode()
+// {
+//   delay(2000); // waiting for cars to go left-right
+//   traffic.turnOnRedDiodeAndOffGreen(GREEN_L, YELLOW_L, RED_L);
+//   traffic.turnOnGreenZebraCrossControlledByTime(GREEN_CROSS_CENTER, RED_CROSS_CENTER);
+//   traffic.turnOnRedDiodeAndOffGreen(GREEN_R, YELLOW_R, RED_R);
+
+//   // turn on zebra lights
+//   traffic.turnOnGreenZebraCrossControlledByTime(GREEN_CROSS_L, RED_CROSS_L);
+//   traffic.turnOnRedZebraCrossControlledByTime(GREEN_CROSS_U, RED_CROSS_U);
+
+//   delay(2000);
+//   traffic.turnOnGreenDiodeAndOffRed(GREEN_U_B, YELLOW_U_B, RED_U_B); // waiting for cars to go up-bottom
+
+//   delay(2000);
+//   traffic.turnOnRedDiodeAndOffGreen(GREEN_U_B, YELLOW_U_B, RED_U_B);
+
+//   traffic.turnOnGreenZebraCrossControlledByTime(GREEN_CROSS_U, RED_CROSS_U);
+//   traffic.turnOnRedZebraCrossControlledByTime(GREEN_CROSS_L, RED_CROSS_L);
+
+//   delay(2000);
+//   traffic.turnOnRedZebraCrossControlledByTime(GREEN_CROSS_CENTER, RED_CROSS_CENTER);
+//   traffic.turnOnGreenDiodeAndOffRed(GREEN_L, YELLOW_L, RED_L);
+//   traffic.turnOnGreenDiodeAndOffRed(GREEN_R, YELLOW_R, RED_R);
+// }
+
+// void setDayModeStartParameters()
+// {
+//   // setting traffic light
+//   digitalWrite(GREEN_L, HIGH);
+//   digitalWrite(GREEN_R, HIGH);
+//   digitalWrite(RED_U_B, HIGH);
+
+//   // setting zebra cross for left up
+//   digitalWrite(RED_CROSS_L, HIGH);
+
+//   // setting zebra cross for down
+//   digitalWrite(GREEN_CROSS_U, HIGH);
+
+//   // setting center zebra cross
+//   digitalWrite(RED_CROSS_CENTER, HIGH);
+// }
 
 void setNightModeStartParameters()
 {
   // setup traffic lights
-  digitalWrite(GREEN_L, HIGH);
-  digitalWrite(GREEN_U_B, HIGH);
+  lightExpander.digitalWrite(EXPANDER2_GREEN_L, HIGH);
+  lightExpander.digitalWrite(EXPANDER2_RED_B, HIGH);
 
   // setup zebra cross
-  digitalWrite(RED_CROSS_L, HIGH);
-  digitalWrite(RED_CROSS_U, HIGH);
-  digitalWrite(RED_CROSS_CENTER, HIGH);
+  crossExpander.digitalWrite(EXPANDER1_RED_CROSS_L, HIGH);
+  crossExpander.digitalWrite(EXPANDER1_GREEN_CROSS_B, HIGH);
 }
