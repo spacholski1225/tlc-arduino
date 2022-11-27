7be4481e5b9914d46c1b0f8974c4f7d6ca4d8367
unknown <spacholski99@gmail.com>
2022-11-27 20:16:04 +0100

    Finally finished project

--- TrafficLightSimulator/TrafficLightSimulator.ino
+++ TrafficLightSimulator/TrafficLightSimulator.ino
@@ -4,6 +4,8 @@
 
 #define TOGGLE_MODE 9
 
+const int TIME_TO_SETUP = 1000;
+
 PCF8574 CE1;
 PCF8574 LE2;
 PCF8574 CE2;
@@ -15,20 +17,16 @@ bool _shouldBeStartUpParams;
 bool _isDayMode = false;
 bool _lastState;
 
-//int switchHelper[500];
-
 void setup()
 {
-  CE1.begin(0x20); //CE1
-  LE2.begin(0x24); // LE2
-  CE2.begin(0x22); // CE2
-  LE1.begin(0x21); //LE1
-
-  Serial.begin(9600);
+  CE1.begin(0x20);
+  LE2.begin(0x24);
+  CE2.begin(0x22);
+  LE1.begin(0x21);
 
   pinMode(TOGGLE_MODE, INPUT_PULLUP);
 
-  //_isDayMode = digitalRead(TOGGLE_MODE) == HIGH;
+  _isDayMode = digitalRead(TOGGLE_MODE) == LOW;
 
   if(_isDayMode)
   {
@@ -45,50 +43,30 @@ void setup()
 
 void loop()
 {
-  Serial.println(digitalRead(TOGGLE_MODE));
-//  int sum = 0;
+  _lastState = _isDayMode;
+  _isDayMode = digitalRead(TOGGLE_MODE);
 
-//  for(int i = 0; i < 500; i++)
-//  {
-//    //Serial.println(digitalRead(TOGGLE_MODE));
-//    switchHelper[i] = digitalRead(TOGGLE_MODE);
-//  }
+  if(_isDayMode)
+  {
+    if(!_lastState && _isDayMode)
+    {
+      _traffic.setUpTrafficLight(CE1, LE2, CE2, LE1);
+      _traffic.setDayModeStartParameters(); 
+      _shouldBeStartUpParams = false;
+      delay(TIME_TO_SETUP);
+    }
+    _traffic.dayMode();
+  }
+  else
+  {
+    if(_lastState && !_isDayMode)
+    {
+      _traffic.setUpTrafficLight(CE1, LE2, CE2, LE1);
+      _traffic.setNightModeStartParameters();
 
-//  for(int i = 0; i < 500; i++)
-//  {  
-//    sum += switchHelper[i];
-
-//    if(sum > 0)
-//    {
-//      _isDayMode = true;
-//    }
-//    else
-//    {
-//      _isDayMode = false;
-//    }
-//  }
-
-//  for(int i = 0; i < 500; i++)
-//  {
-//    switchHelper[i] = 0;
-//  }
- 
-
-//  if(_isDayMode)
-//  {
-//    Serial.println("tryb dzien");
-//  }
-//  else
-//  {
-//    Serial.println("tryb noc");
-//  }
-  
-//   if (_isDayMode)
-//   {
-//     _traffic.dayMode();
-//   }
-//   else
-//   {
-//     _traffic.nightMode();
-//   }
+      _shouldBeStartUpParams = false;
+      delay(TIME_TO_SETUP);
+    }
+    _traffic.nightMode();
+  }
 }
